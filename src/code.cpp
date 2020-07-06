726f72d82786f56a316d9cd5337b8f1b07c68fba
Sergey Lazarev <lasest1999@gmail.com>
2020-07-07 00:15:54 +0500

    Moved help message to a separate function

--- src/code.cpp
+++ src/code.cpp
@@ -11,58 +11,14 @@ const vector<uint8_t***> images;
 
 //Returns all colors of the image
 //crit_error - max divergence from the standart color
-vector<pair<int, int>> calcColors(uint8_t*** image, int height, int width, double crit_error)
-{
-	vector<pair<int, int>> result;
-	double min_dis = 99999999999999;
-	double dis = 0;
-	int index = 0;
-	double red_sq;
-	double green_sq;
-	double blue_sq;
-	result.push_back(make_pair(index, 0));
-	for(int i = 0; i < height; i++)
-	{
-		for(int j = 0; j < width; j++)
-		{
-			//Calc distance between the color of the pixel and the standart color
-			for(int k = 0; k < colors.size(); k++)
-			{
-				red_sq = pow(image[i][j][0] - colors[k].red, 2);
-				green_sq = pow(image[i][j][1] - colors[k].green, 2);
-				blue_sq = pow(image[i][j][2] - colors[k].blue, 2);
-				dis = pow((red_sq + green_sq + blue_sq), 0.5);
-				if(dis < min_dis)
-				{
-					min_dis = dis;
-					index = k;
-					if(dis <= crit_error || dis == 0)
-					{
-						break;
-					}
-				}
-			}
-			//Write the result to the vector
-			for(int k = 0; k < result.size(); k++)
-			{
-				if(result[k].first == index)
-				{
-					result[k].second++;
-					break;
-				}
-				if(k == result.size() - 1)
-				{
-					result.push_back(make_pair(index, 1));
-				}
-			} 
-			min_dis = 99999999999999;
-		}
-	}
-	return result;
+
+void show_help_message(char* program_name){
+    cout << endl;
+        
+    // TODO: Add actual usage info after main functionality is implemented
+    cout << "Usage: " << program_name << " [filenames]" << endl << endl;
+    cout << "A simple program to calculate the projective cover of grass or foliage projective cover by a photo." << endl;    
 }
-
-
-
 
 bool execute(string command)
 {
@@ -101,11 +57,7 @@ int main(int argc, char *argv[])
 {	
     // Display help message and quit
     if ((argc > 1) && (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0)) {
-        cout << endl;
-        
-        // TODO: Add actual usage info after main functionality is implemented
-        cout << "Usage: " << argv[0] << " [filenames]" << endl << endl;
-        cout << "A simple program to calculate the projective cover of grass or foliage projective cover by a photo." << endl;
+        show_help_message(argv[0]);
         return 0;
     }   
     
